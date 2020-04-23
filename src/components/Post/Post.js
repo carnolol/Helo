e9d02e2ce32008deec06905e1c07890632420f81
Mike Chadwick <Harakili@gmail.com>
2020-04-23 00:44:52 -0600

    dfsdf

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -9,18 +9,22 @@ export class Post extends Component {
         }
     }
     componentDidMount = () => {
+        // let id = this.props.match.params.postid
+        // let newId = parseInt(id)
         axios.get(`/api/post/${this.props.match.params.postid}`).then(res => {
             console.log('i am the data',res.data)
+            console.log('i am the props', typeof this.props.match.params.postid)
             this.setState({
                 post: res.data
             })
         })   
     }
     render() {
+        console.log('state:' , this.state.post)
         return (
             <div>
               When a post is clicked on this will display the post 
-        <h1>{this.state.title}</h1>
+        <h1>{this.state.post.title}</h1>
             </div>
         )
     }
